e0fa2d60ba9ba3b2fe1dbaf8eec7396edeaf6334
Beep-88 <79515051+Beep-88@users.noreply.github.com>
2021-05-17 16:08:55 +0200

    Add files via upload

---
+++ java.js
@@ -0,0 +1,28 @@
+function popup(){
+	alert("Indiquer directement l'action à effectuer est aussi une option.");
+}
+
+function souris_entre1(){
+	document.querySelector("#js1").classList.remove("off");
+	document.querySelector("#js1").classList.add("on");
+}
+function souris_sort1(){
+	document.querySelector("#js1").classList.remove("on");
+	document.querySelector("#js1").classList.add("off");
+}
+function souris_entre2(){
+	document.querySelector("#js2").classList.remove("off");
+	document.querySelector("#js2").classList.add("on");
+}
+function souris_sort2(){
+	document.querySelector("#js2").classList.remove("on");
+	document.querySelector("#js2").classList.add("off");
+}
+function souris_entre3(){
+	document.querySelector("#js3").classList.remove("off");
+	document.querySelector("#js3").classList.add("on");
+}
+function souris_sort3(){
+	document.querySelector("#js3").classList.remove("on");
+	document.querySelector("#js3").classList.add("off");
+}
